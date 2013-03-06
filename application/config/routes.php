8be79696db162cd947826d28fc514c7ef843b325
RmzVoid <rmzvoid@ya.ru>
2013-03-06 18:06:15 +0400

    добавлен аншарп маск если изображение существует, отдаем файл минуя контроллер

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,6 +3,8 @@
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $route['default_controller'] = 'imagecache';
+
+// to trim trailing .jpg
 $route['(:any)/(:any).jpg'] = 'imagecache/$1/$2';
 
 ?>
